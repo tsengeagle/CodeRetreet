8b1fa303366d4f829db2d6b739e5e38a0739c19e
曾義格 <tsengeagle@gmail.com>
2016-12-05 23:04:35 +0800

    first two test case

--- GoodVSEvil/GoodVSEvilTests.cs
+++ GoodVSEvil/GoodVSEvilTests.cs
@@ -8,27 +8,42 @@ using System.Threading.Tasks;
 namespace GoodVSEvil
 {
 
+    //Middle Earth is about to go to war.The forces of good will have many battles with the forces of evil.Different races will certainly be involved.Each race has a certain 'worth' when battling against others. On the side of good we have the following races, with their associated worth:
+    //Good
+    //Hobbits - 1
+    //Men - 2
+    //Elves - 3
+    //Dwarves - 3
+    //Eagles - 4
+    //Wizards - 10
+
+    //Evil
+    //Orcs - 1
+    //Men - 2
+    //Wargs - 2
+    //Goblins - 2
+    //Uruk Hai - 3
+    //Trolls - 5
+    //Wizards - 10
+
     //The function will be given two parameters.Each parameter will be a string separated by a single space.Each string will contain the count of each race on the side of good and evil.
     //The first parameter will contain the count of each race on the side of good in the following order:
     //Hobbits, Men, Elves, Dwarves, Eagles, Wizards.
     //The second parameter will contain the count of each race on the side of evil in the following order:
     //Orcs, Men, Wargs, Goblins, Uruk Hai, Trolls, Wizards.
     //All values are non-negative integers. The resulting sum of the worth for each side will not exceed the limit of a 32-bit integer.
-
-
+    
     //Output:
-
-
     //Return ""Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Evil eradicates all trace of Good" if evil wins, or "Battle Result: No victor on this battle field" if it ends in a tie.
     [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
-    class GoodVSEvilTests
+    public class GoodVSEvilTests
     {
         [TestMethod]
         public void InputArmy_ShouldReturnGoodWin()
         {
             //arrange
-            var good = "Hobbits, Men, Elves, Dwarves, Eagles, Wizards";
-            var evil = "Orcs, Men, Wargs, Goblins, Uruk Hai, Trolls, Wizards";
+            var good = "Men"; //2
+            var evil = "Orcs"; //1
 
             var target = new GoodVSEvil.Match();
             //act
@@ -38,13 +53,34 @@ namespace GoodVSEvil
 
             Assert.AreEqual("Battle Result: Good triumphs over Evil", result);
         }
+
+        [TestMethod]
+        public void InputArmy_ShouldReturnEvilWin()
+        {
+            //arrange
+            var good = "Hobbits"; //1
+            var evil = "Men"; //2
+
+            var target = new GoodVSEvil.Match();
+            //act
+            var result = target.GetResult(good, evil);
+
+            //assert
+
+            Assert.AreEqual("Battle Result: Evil eradicates all trace of Good", result);
+        }
+
     }
 
     internal class Match
     {
         public string GetResult(string good, string evil)
         {
-            return "";
+            if (good=="Hobbits" && evil=="Men")
+            {
+                return "Battle Result: Evil eradicates all trace of Good";
+            }
+            return "Battle Result: Good triumphs over Evil";
         }
     }
 }
