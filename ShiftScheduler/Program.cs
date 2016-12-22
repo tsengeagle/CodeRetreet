a879e0ddf2d106f6985b736a9708a02526785521
曾義格 <tsengeagle@gmail.com>
2016-12-22 16:31:49 +0800

    test case for shift not fit one month

--- ShiftScheduler/Program.cs
+++ ShiftScheduler/Program.cs
@@ -57,6 +57,92 @@ namespace ShiftScheduler
             Assert.IsFalse(actualStatus);
         }
 
+        [TestMethod]
+        public void ValidatorTest_Shift20Days_ShouldFail()
+        {
+            var shifts = MakeShifts5DaysOn2DaysOffBut20Days();
+
+            var target = new ScheduleValidator(shifts);
+            var actualStatus = target.CheckIfValid();
+
+            Assert.IsFalse(actualStatus);
+        }
+
+        [TestMethod]
+        public void ValidatorTest_ShiftCrossMonth_ShouldFail()
+        {
+            var shifts = MakeShifts5DaysOn2DaysOffButCrossMonth();
+
+            var target = new ScheduleValidator(shifts);
+            var actualStatus = target.CheckIfValid();
+
+            Assert.IsFalse(actualStatus);
+        }
+
+        private List<Shift> MakeShifts5DaysOn2DaysOffButCrossMonth()
+        {
+            return new List<Shift>()
+            {
+                new Shift() {Date=new DateTime(2017,1,1),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,2),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,3),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,4),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,5),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,6),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,7),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,8),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,9),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,10),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,11),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,12),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,13),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,14),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,15),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,16),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,17),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,18),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,19),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,20),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,21),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,22),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,23),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,24),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,25),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,26),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,27),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,28),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,29),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,30),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,31),Type="上班" },
+                new Shift() {Date=new DateTime(2017,2,1),Type="上班" },
+            };
+        }
+        private List<Shift> MakeShifts5DaysOn2DaysOffBut20Days()
+        {
+            return new List<Shift>()
+            {
+                new Shift() {Date=new DateTime(2017,1,1),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,2),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,3),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,4),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,5),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,6),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,7),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,8),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,9),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,10),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,11),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,12),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,13),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,14),Type="休假" },
+                new Shift() {Date=new DateTime(2017,1,15),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,16),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,17),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,18),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,19),Type="上班" },
+                new Shift() {Date=new DateTime(2017,1,20),Type="休假" },
+            };
+        }
         private List<Shift> MakeShifts6DaysOn1DaysOff()
         {
             return new List<Shift>()
@@ -188,18 +274,27 @@ namespace ShiftScheduler
         public bool CheckIfValid()
         {
             var checkResult = true;
-            foreach (var shift in _shifts.OrderByDescending(o=>o.Date))
+
+            var firstShiftDate = _shifts.First().Date;
+            var nextMonth=firstShiftDate.AddMonths(1);
+            var shouldShiftDays = (nextMonth - firstShiftDate).Days;
+            if (_shifts.Count()!=shouldShiftDays)
             {
-                var weekShifts=_shifts.Where(w=>w.Date<=shift.Date).OrderByDescending(o=>o.Date).Take(7);
-                if (weekShifts.Count()==7)
+                checkResult = false;
+            }
+
+            foreach (var shift in _shifts.OrderByDescending(o => o.Date))
+            {
+                var weekShifts = _shifts.Where(w => w.Date <= shift.Date).OrderByDescending(o => o.Date).Take(7);
+                if (weekShifts.Count() == 7)
                 {
-                    if (weekShifts.Where(w=>w.Type=="休假").Count()<2)
+                    if (weekShifts.Where(w => w.Type == "休假").Count() < 2)
                     {
                         checkResult = false;
                     }
                 }
             }
-            
+
             return checkResult;
         }
     }
